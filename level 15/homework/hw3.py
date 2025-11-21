126c9d9ac7134f79e224ce08e8b6a6e2a0dad6dc
salogiorgadze <salome.giorgadze0511@gmail.com>
2025-11-21 18:33:09 +0400

    second commit

---
+++ level 15/homework/hw3.py
@@ -0,0 +1,7 @@
+i = 0
+while i <= 20:
+    if i % 2 == 0:
+        print(i,"Couple")
+    else:
+        print(i,"Single")
+    i += 1
---
+++ level 15/homework/hw8.py
@@ -0,0 +1,6 @@
+i = 0
+total = 0
+while i <= 20:
+    total += i
+    i += 2
+print(total)
