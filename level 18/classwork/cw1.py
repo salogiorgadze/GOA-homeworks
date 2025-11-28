aa0a3130de752ca033b33230ec84b6d5e8eb09c5
salogiorgadze <salome.giorgadze0511@gmail.com>
2025-11-29 00:16:23 +0400

    fourteenth commit

---
+++ level 18/classwork/cw1.py
@@ -0,0 +1,9 @@
+#პარამეტრს რასაც გადავცემთ იმაზე იმუშავებს ფუნქცია
+def multiply(x,y):
+    return x*y
+print(multiply(2,10))
+
+def opposite(number):
+        return - number
+print(opposite(15))
+print(opposite(-16))
